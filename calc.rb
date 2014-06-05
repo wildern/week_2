09a721de59ae4728ac387cddc2ba94739fab8cf8
bkzhn <absh.bktl@gmail.com>
2014-06-06 02:58:45 +0600

    Update week 2

--- calc.rb
+++ calc.rb
@@ -4,16 +4,66 @@
 
 
 def sum
+=begin
+	(int, int) -> int
+
+	this method has to accept two integers
+	and return one integer (sum of them)
+
+	>  sum(1, 2)
+	=> 3
+=end
+	# write your code here
 end
 
 def difference
+=begin
+	(int, int) -> int
+
+	this method has to accept two integers
+	and return one integer (difference of them)
+
+	>  difference(5, 2)
+	=> 3
+=end
+	# write your code here
 end
 
 def product
+=begin
+	(int, int) -> int
+
+	this method has to accept two integers
+	and return one integer (product of them)
+
+	>  product(5, 2)
+	=> 10
+=end
+	# write your code here
 end
 
 def quotient
+=begin
+	(int, int) -> int
+
+	this method has to accept two integers
+	and return one integer (quotient of them)
+
+	>  quotient(6, 2)
+	=> 3
+=end
+	# write your code here
 end
 
 def factorial
+=begin
+	(int) -> int
+
+	this method has to accept an integer
+	and return one integer (factorial of it)
+
+	>  factorial(5)
+	=> 120
+=end
+	# write your code here
 end
